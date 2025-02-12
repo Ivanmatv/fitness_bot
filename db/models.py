d1f5dfa4fdcb07058a0e93e57225609ae13d296f
unknown <immatveev@akontech.ru>
2025-02-12 09:12:53 +0300

    modify models.py

--- db/models.py
+++ db/models.py
@@ -51,7 +51,7 @@ class Exercise(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    type = Column(String, nullable=False)
+    type = Column(String, nullable=False) # базовое или подсобное
     description = Column(String, nullable=True)
     image_url = Column(String, nullable=True)  # Ссылка на изображение упражнения
     sport_id = Column(Integer, ForeignKey('sports.id'), nullable=False)
